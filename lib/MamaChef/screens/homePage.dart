7fdc60cb2ffedab2df438c71eb24131d12906a05
Esteban LE CALVEZ <esteban.le-calvez@kurmi-software.com>
2019-09-25 15:23:19 +0200

    Modif homepage

--- lib/MamaChef/screens/homePage.dart
+++ lib/MamaChef/screens/homePage.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:miamitymds/CommonPages/whatIsNearMePage.dart';
+import 'package:miamitymds/MamaChef/screens/addPlatPage.dart';
+import 'package:miamitymds/MamaChef/screens/myAccountPage.dart';
 import 'package:miamitymds/Widgets/MiamityAppBar.dart';
 import 'package:miamitymds/Widgets/MiamityButton.dart';
 import 'package:miamitymds/auth.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({this.title, this.auth, this.onSignedOut});
@@ -21,9 +25,104 @@ class HomeScreenState extends State<HomeScreen> {
       ),
       body: Container(
           child: Center(
-        child: Column(
+        child: ListView(
           children: <Widget>[
-            Text("Accueil"),
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                ),
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: FadeInImage.memoryNetwork(
+                        height: 200,
+                        width: 200,
+                        placeholder: kTransparentImage,
+                        fadeInDuration: const Duration(seconds: 1),
+                        fit: BoxFit.cover,
+                        image:
+                            "https://p8.storage.canalblog.com/88/72/717345/113739915.jpg",
+                      ),
+                    ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                ),
+                MiamityButton(
+                    title: "J'AJOUTE UN PLAT",
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AddPlate(
+                                  auth: widget.auth,
+                                )),
+                      );
+                    }),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                ),
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: FadeInImage.memoryNetwork(
+                        height: 200,
+                        width: 200,
+                        placeholder: kTransparentImage,
+                        fadeInDuration: const Duration(seconds: 1),
+                        fit: BoxFit.cover,
+                        image:
+                            "https://p8.storage.canalblog.com/88/72/717345/113739915.jpg",
+                      ),
+                    ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                ),
+                MiamityButton(
+                  title: "JE REGARDE LES PLATS PROCHE DE MOI",
+                  onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => WhatIsNearMePage(
+                                  auth: widget.auth,
+                                )),
+                      );
+                    }
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                ),
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child:  FadeInImage.memoryNetwork(
+                        height: 200,
+                        width: 200,
+                        placeholder: kTransparentImage,
+                        fadeInDuration: const Duration(seconds: 1),
+                        fit: BoxFit.cover,
+                        image:
+                            "https://p8.storage.canalblog.com/88/72/717345/113739915.jpg",
+                      ),
+                    ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                ),
+                MiamityButton(
+                  title: "JE MODIFIE MON COMPTE",
+                  onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => MyAccountPage(
+                                  auth: widget.auth,
+                                )),
+                      );
+                    }
+                ),
+              ],
+            )
           ],
         ),
       )),
